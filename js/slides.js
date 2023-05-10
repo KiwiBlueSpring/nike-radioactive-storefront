2ea12e514b35933b2c1bf4d0e9cb2f07bab55cae
KiwiBlueSpring <72439854+KiwiBlueSpring@users.noreply.github.com>
2023-05-11 10:25:04 +1200

    Update site

--- js/slides.js
+++ js/slides.js
@@ -1,7 +1,8 @@
-// Js to control slides positioning
+// Flow based slides - for screens and going 'next'
 let currentStep = 0
+const totalSlides = 2;
 
-function nextSlide() {
+function nextSlid1() {
     currentStep = (currentStep+1) 
     document.querySelector('.slidesWrapper').style.transform = `translateX(-${currentStep * 100}%)`;
 }
@@ -16,4 +17,28 @@ function reset() {
     currentStep = 0
     document.querySelector('.slidesWrapper').style.transform = `translateX(-${currentStep * 100}%)`;
 }
+
+function nextSlide() {
+    currentStep = (currentStep + 1) % totalSlides; // This will reset currentStep to 0 when it reaches totalSlides
+    document.querySelector('.slidesWrapper').style.transform = `translateX(-${currentStep * 100}%)`;
+}
+
+
+
+// Automatic Slides
+
+let currentSlide = 0
+// const numSlides = document.querySelector('.slidesWrapper').childElementCount
+let numSlides = 2
+
+
+setInterval(()=>{
+
+    currentSlide = (currentSlide+1) % numSlides;
+    document.querySelector('.slidesWrapper').style.transform = `translateX(-${currentSlide * 100}%)`;
+
+
+},2000)
+
+
 
