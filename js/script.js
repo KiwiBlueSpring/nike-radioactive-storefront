76f76790494bfc4022cf2c721f0df14c1e643988
KiwiBlueSpring <72439854+KiwiBlueSpring@users.noreply.github.com>
2023-05-11 17:17:28 +1200

    Develop Studio.html: - Add customisation platters - Added intial platter functionality - Edit & develop script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+// get all the customisationPlatter elements
+let platters = document.querySelectorAll('.customisationPlatter');
+
+console.log(platters)
+
+// loop through all the platters and add an event listener
+platters.forEach(function(platter) {
+    platter.addEventListener('click', function() {
+        // get the optionRow within the clicked platter
+        let optionRow = this.querySelector('.optionRow');
+
+        // toggle the display of the optionRow
+        if (optionRow.style.display === "none") {
+            optionRow.style.display = "flex";
+        } else {
+            optionRow.style.display = "none";
+        }
+    });
+
+    // get all the options within the platter
+    let options = platter.querySelectorAll('.sizeOption, .colourOption');
+
+    // loop through the options and add an event listener
+    options.forEach(function(option) {
+        option.addEventListener('click', function(event) {
+            // prevent the click event from bubbling up to the platter
+            event.stopPropagation();
+
+            // hide the optionRow when an option is clicked
+            this.parentNode.style.display = "none";
+        });
+    });
+});
+});
